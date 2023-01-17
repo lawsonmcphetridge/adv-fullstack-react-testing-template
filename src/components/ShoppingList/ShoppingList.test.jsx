fe31f33f6632a251b14634eb2a18961faafd7a7e
Lawson <lawsonmcphetridge2020@gmail.com>
2023-01-17 12:37:05 -0800

    passed shoppinglist test

---
+++ src/components/ShoppingList/ShoppingList.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import ShoppingList from './ShoppingList';
+
+describe('ShoppingList', () => {
+  it.only('Displays Shoppinglist name', () => {
+    const shoppingList = {
+      id: 1,
+      name: 'My Cool List',
+      shoppingItems: [],
+    };
+
+    render(<ShoppingList shoppingList={shoppingList} />);
+    expect(
+      screen.queryByTestId('shopping-list-name-1').textContent
+    ).toEqual('My Cool List');
+  });
+});
